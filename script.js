cb634577df68d31112cc0977ee24f1de93f03288
JLeow00 <starboileow@gmail.com>
2015-12-26 13:54:21 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+var lat = $("#latitude");
+var lon = $("#longitude");
+$(document).ready(function() {
+	$("#geolocButton").click(function() {
+		getLocation();
+	});
+});
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        lat.text("Geolocation is not supported by this browser.");
+    }
+}
+
+
+function showPosition(position) {
+    lat.text("Latitude: " + position.coords.latitude);
+    lon.text("Longitude: " + position.coords.longitude);
+    var src = "https://www.google.com/maps/embed/v1/place?key=AIzaSyDM-OQH5nB6-x2KHGhabtCEe5kg6JgK5Qc&q=" +
+    position.coords.latitude + "," +
+    position.coords.longitude;
+    alert(src);
+    $("#gmaps").attr("src", src);
+}
